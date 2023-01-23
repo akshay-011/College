55f408faac0c486c4ccf85b3f48198141d5a5c6f
Akshay Kumar <102289107+akshay-011@users.noreply.github.com>
2023-01-23 22:26:54 +0530

    Create exp1.java

---
+++ OOPS/exp1.java
@@ -0,0 +1,9 @@
+import java.lang.Math;
+class exp1{
+    public static void main(String args[]){
+        double x = 5;
+        double y;
+        y = Math.sqrt(x) ;
+        System.out.println ("y = " + y);
+    }
+}
